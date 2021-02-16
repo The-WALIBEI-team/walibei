bf50f9535f42343186fd81b4608e31fbcf491e8b
Samiratou Bonkoungou <bonkoungousamiratou75@gmail.com>
2021-02-17 01:30:43 +0530

    home

--- walibei/src/app/app-routing.module.ts
+++ walibei/src/app/app-routing.module.ts
@@ -3,12 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/components/login/login.component';
 import { RegisterComponent } from './auth/components/register/register.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { ProduitComponent } from './produit/produit.component';
+import { ServiceComponent } from './service/service.component';
+import { EmploiComponent } from './emploi/emploi.component';
+import { LivraisonComponent } from './livraison/livraison.component';
+
 
 const routes: Routes = [
   {path:  "", pathMatch:  "full",redirectTo:  "accueil"},
   {path: 'accueil', component: AccueilComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'produit', component: ProduitComponent},
+  {path: 'service', component: ServiceComponent},
+  {path: 'emploi', component: EmploiComponent},
+  {path: 'livraisom', component: LivraisonComponent}
+  
 ];
 
 @NgModule({
